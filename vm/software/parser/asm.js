35d281fa7460d78f2a017e5ce2266809f5d68bac
HenryTy88 <56137629+HenryTy88@users.noreply.github.com>
2022-08-12 00:41:55 -0400

    it finally works
    turns out I can't write code better than the tutorial guy

--- vm/software/parser/asm.js
+++ vm/software/parser/asm.js
@@ -4,7 +4,7 @@ const asType = type => value => ({type, value});
 const mapJoin = parser => parser.map(items => items.join(''));
 const peek = A.lookAhead(A.regex(/^./))
 const last = a => a[a.length-1]
-const typifyBracketedExpression = expr => asType('BRACKETED_EXPRESSION')(expr.map(element => Array.isArray(element) ? typifyBracketedExpression(element) : element)) //wtf
+const typifyBracketedExpression = expr => asType('BRACKETED_EXPRESSION')(expr.map(element => Array.isArray(element) ? typifyBracketedExpression(element) : element)); //wtf
 
 const upperOrLowerStr = s => A.choice([
     A.str(s.toLowerCase()),
@@ -44,70 +44,69 @@ const variable = A.char('!').chain(() => validIdentifier).map(asType('VARIABLE')
 
 const bracketExpr = A.contextual(function* () {
     const states = {
-        OPERATOR_OR_CLOSING_BRACKET: 0,
-        ELEMENT_OR_OPENING_BRACKET: 1,
-    }
-
-    let expr = []
-    let stack = []
-    let currentState = states.ELEMENT_OR_OPENING_BRACKET
-
+      OPEN_BRACKET: 0,
+      OPERATOR_OR_CLOSING_BRACKET: 1,
+      ELEMENT_OR_OPENING_BRACKET: 2,
+      CLOSE_BRACKET: 3
+    };
+  
+    let state = states.ELEMENT_OR_OPENING_BRACKET;
+  
+    const expr = [];
+    const stack = [expr];
+    yield A.char('(');
+  
     while (true) {
-        const nextChar = yield peek
-        console.log(`EXPR: ${JSON.stringify(expr, null, 4)}`)
-        console.log(`STACK: JSON.stringify(stack, null, 4)`)
-        console.log(nextChar)
-        
-        switch(currentState) {
-            case (states.ELEMENT_OR_OPENING_BRACKET): {
-                switch (nextChar) {
-                    case (')'): {yield A.fail('Unexpected end of expression'); break;}
-                    case ('('): {
-                        yield A.char('(')
-                        yield A.optionalWhitespace
-        
-                        expr.push([])
-                        stack.push(last(expr))
-        
-                        currentState = states.ELEMENT_OR_OPENING_BRACKET
-                        break;
-                    }
-                    default: {
-                        last(stack).push(yield A.choice([hexLiteral, variable])); 
-                        yield A.optionalWhitespace
-
-                        currentState = states.OPERATOR_OR_CLOSING_BRACKET    
-                        break
-                    }
-                }
-                break
-            }
-
-            case (states.OPERATOR_OR_CLOSING_BRACKET): {
-                switch(nextChar) {
-                    case(')'): {
-                        yield A.char(')')
-                        yield A.optionalWhitespace
-                        
-                        stack.pop()
-                        if(stack.length == 0) { return typifyBracketedExpression(expr)}
-
-                        currentState = states.OPERATOR_OR_CLOSING_BRACKET
-                        break;
-                    }
-
-                    default: {
-                        last(stack).push(yield operator)
-                        yield A.optionalWhitespace
-
-                        currentState = states.ELEMENT_OR_OPENING_BRACKET
-                        break;
-                    }
-                    }
-                }
-            }
-        }  
-    }) 
+      const nextChar = yield peek;
+  
+      if (state === states.OPEN_BRACKET) {
+        yield A.char('(');
+        expr.push([]);
+        stack.push(last(expr));
+        yield A.optionalWhitespace;
+        state = states.ELEMENT_OR_OPENING_BRACKET;
+      } else if (state === states.CLOSE_BRACKET) {
+        yield A.char(')');
+        stack.pop();
+        if (stack.length === 0) {
+          // We've reached the end of the bracket expression
+          break;
+        }
+  
+        yield A.optionalWhitespace;
+        state = states.OPERATOR_OR_CLOSING_BRACKET;
+      } else if (state === states.ELEMENT_OR_OPENING_BRACKET) {
+        if (nextChar === ')') {
+          yield A.fail('Unexpected end of expression');
+        }
+  
+        if (nextChar === '(') {
+          state = states.OPEN_BRACKET;
+        } else {
+          last(stack).push(yield A.choice([
+            hexLiteral,
+            variable
+          ]));
+          yield A.optionalWhitespace;
+          state = states.OPERATOR_OR_CLOSING_BRACKET;
+        }
+      } else if (state === states.OPERATOR_OR_CLOSING_BRACKET) {
+        if (nextChar === ')') {
+          state = states.CLOSE_BRACKET;
+          continue;
+        }
+  
+        last(stack).push(yield operator);
+        yield A.optionalWhitespace;
+        state = states.ELEMENT_OR_OPENING_BRACKET;
+      } else {
+        // This shouldn't happen!
+        throw new Error('Unknown state');
+      }
+  
+    }
+    return typifyBracketedExpression(expr);
+  });
 
 const squareBrakExpr = A.contextual(function* () {
     yield A.char('[')
@@ -129,24 +128,24 @@ const squareBrakExpr = A.contextual(function* () {
                     variable,
                     bracketExpr,
                 ]));
-                
-                currentState = states.EXPECT_OPERATOR;
                 yield A.optionalWhitespace;
-                continue
+                currentState = states.EXPECT_OPERATOR;
+
+                break;
             }
             case (states.EXPECT_OPERATOR): {
                 const nextChar = yield peek;
                 if (nextChar == ']') {
                     yield A.char(']')
                     yield A.optionalWhitespace;
-                    return asType('SQAURE_BRACKET_EXPRESSION')(expr)
+                    return asType('SQUARE_BRACKET_EXPRESSION')(expr)
                 }
                 else {
                     expr.push(yield operator)
                     currentState = states.EXPECT_ELEMENT
                     yield A.optionalWhitespace
                 }
-                continue;
+                break;
             }
         }
     }  
@@ -177,7 +176,7 @@ const movLitToRegister = A.contextual(function* () {
 const testInstruction = movLitToRegister.run('mov $12, acc')
 const bracketInstruction = movLitToRegister.run('mov [$43 + !loc - $07], x')
 const nestedInstruction = movLitToRegister.run('mov [$42 + !loc - ($05 * !var) - $07], y')
-const nestedHell = movLitToRegister.run('mov [$42 + (!loc - ($05 * ($31 + !var) - $07)], d')
+const nestedHell = movLitToRegister.run('mov [$42 + !loc - ($05 * ($31 + !var) - $07)], d')
 
 console.log(JSON.stringify(testInstruction, null, 4))
 console.log(JSON.stringify(bracketInstruction, null, 4))
